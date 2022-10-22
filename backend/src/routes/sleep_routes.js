3c07df240aa901ab664afd1ddd80974d54c58a69
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-10-22 18:44:16 +0000

    Completed the routes, controllers and models for the sleep module

--- backend/src/routes/sleep_routes.js
+++ backend/src/routes/sleep_routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const Sleep = require("../models/sleep_models");
 const sleep_controller = require("../controllers/sleep_controllers");
 const sleep_router = express.Router();
 
@@ -7,7 +6,10 @@ sleep_router.post("/", sleep_controller.sleep_create);
 
 sleep_router.get("/:sleep_id", sleep_controller.sleep_get_by_sleep_id);
 
-sleep_router.get("/:user_id", sleep_controller.sleep_get_all_by_user_id);
+sleep_router.get(
+  "/users/:username",
+  sleep_controller.sleep_get_all_by_username
+);
 
 sleep_router.patch(
   "/:sleep_id",
