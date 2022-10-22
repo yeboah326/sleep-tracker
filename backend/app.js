50f3264fff5545bf399dc545c0f9c2f028eece18
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-10-22 11:55:54 +0000

    Add database configurations

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const logger = require("morgan");
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 // Constants
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT || "3000";
+const DATABASE_USERNAME = process.env.DATABASE_USERNAME || "username";
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "password";
+const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
+const DATABASE_PORT = process.env.DATABASE_PORT || "27017";
+
+// Database connection
+mongoose.connect(
+  `mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/`
+);
 
 // Routers
 const user_router = require("./src/routes/user_routes");
 const sleep_router = require("./src/routes/sleep_routes");
 
 // Add middleware to parse json requests body
-app.use(express.json())
+app.use(express.json());
 
 // Add logging functionality to the app
 app.use(logger("short"));
