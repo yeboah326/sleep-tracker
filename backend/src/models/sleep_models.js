baf0121a976f83bc690641293086b6f9d0924673
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-10-22 13:43:18 +0000

    Add helper methods, models and routes for sleep module

--- backend/src/models/sleep_models.js
+++ backend/src/models/sleep_models.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const sleep_schema = mongoose.Schema({
   sleep_start: { type: Date, default: Date.now },
   sleep_stop: { type: Date, default: Date.now },
-  sleep_duration: { type: Number },
   username: { type: String },
+  sleep_duration_hours: { type: Number },
+  sleep_duration_minutes: { type: Number },
+  sleep_duration_seconds: { type: Number },
 });
 
 const Sleep = mongoose.model("Sleep", sleep_schema);
