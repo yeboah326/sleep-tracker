e65793d5cd88daf8e539d4b2cdbea575f83018d1
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-10-22 08:45:02 +0000

    Add files to the controllers

---
+++ backend/src/controllers/sleep_controllers.js
@@ -0,0 +1,17 @@
+async function sleep_get_by_sleep_id(req, res, next) {}
+
+async function sleep_get_all_by_user_id(req, res, next) {}
+
+async function sleep_update_by_sleep_sleep_id(req, res, next) {}
+
+async function sleep_delete_by_sleep_id(req, res, next) {}
+
+async function sleep_delete_all_by_user_id(req, res, next) {}
+
+module.exports = {
+  sleep_get_by_sleep_id,
+  sleep_get_all_by_user_id,
+  sleep_update_by_sleep_sleep_id,
+  sleep_delete_by_sleep_id,
+  sleep_delete_all_by_user_id,
+};
