a0bdfe0933a3a3f871502ab5c9a96003e2ec057a
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-10-22 20:34:01 +0000

    Complete the implementation of the user routes, controllers and models

--- backend/src/models/user_models.js
+++ backend/src/models/user_models.js
@@ -1,12 +1,43 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+
+const SALT_FACTOR = parseInt(process.env.SALT_FACTOR);
 
 const user_schema = mongoose.Schema({
   first_name: { type: String },
-  middle_name: { type: String },
   last_name: { type: String },
   password: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
 });
 
-const User = mongoose.model("User", user_schema);
+user_schema.pre("save", function (done) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return done();
+  }
+  bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+    if (err) return done(err);
+    bcrypt.hash(user.password, salt, function (err, hashedPassword) {
+      if (err) return done(err);
+      user.password = hashedPassword;
+      done();
+    });
+  });
+});
+
+user_schema.methods.check_password = function (guess) {
+  bcrypt.compare(guess, this.password, function (err, isMatch) {
+    if (err) {
+      return err;
+    } else {
+      console.log("Password is correct");
+      return { isMatch };
+    }
+  });
+};
+
+const User = mongoose.model("sleep_tracker_user", user_schema);
+
+module.exports = User;
--- backend/src/routes/user_routes.js
+++ backend/src/routes/user_routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const user_controllers = require("../controllers/user_controller");
 const user_router = express.Router();
 
-user_router.post("/", user_controllers.user_signup);
+user_router.post("/signup", user_controllers.user_signup);
 
-user_router.post("/", user_controllers.user_login);
+user_router.post("/login", user_controllers.user_login);
 
 user_router.get("/:user_id", user_controllers.user_get_info_by_user_id);
 
