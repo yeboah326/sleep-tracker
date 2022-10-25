e377148ae85ffa3b902960d94a5f644b9ea0d588
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-10-25 08:02:15 +0000

    Add authenticate token middle to sleep endpoints

--- backend/src/routes/sleep_routes.js
+++ backend/src/routes/sleep_routes.js
@@ -1,23 +1,38 @@
 const express = require("express");
 const sleep_controller = require("../controllers/sleep_controllers");
+const authenticate_token = require("../middleware/authenticate_token");
 const sleep_router = express.Router();
 
-sleep_router.post("/", sleep_controller.sleep_create);
+sleep_router.post("/", authenticate_token, sleep_controller.sleep_create);
 
-sleep_router.get("/:sleep_id", sleep_controller.sleep_get_by_sleep_id);
+sleep_router.get(
+  "/:sleep_id",
+  authenticate_token,
+  sleep_controller.sleep_get_by_sleep_id
+);
 
 sleep_router.get(
   "/users/:username",
+  authenticate_token,
   sleep_controller.sleep_get_all_by_username
 );
 
 sleep_router.patch(
   "/:sleep_id",
+  authenticate_token,
   sleep_controller.sleep_update_by_sleep_sleep_id
 );
 
-sleep_router.delete("/:sleep_id", sleep_controller.sleep_delete_by_sleep_id);
+sleep_router.delete(
+  "/:sleep_id",
+  authenticate_token,
+  sleep_controller.sleep_delete_by_sleep_id
+);
 
-sleep_router.delete("/:user_id", sleep_controller.sleep_delete_all_by_user_id);
+sleep_router.delete(
+  "/:user_id",
+  authenticate_token,
+  sleep_controller.sleep_delete_all_by_user_id
+);
 
 module.exports = sleep_router;
