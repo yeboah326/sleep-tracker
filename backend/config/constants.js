298d03546d34d7a205b4886c9753b01c5d7f5a07
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-10-26 09:42:35 +0000

    Remove all environment variables and place them in one file

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 
+const PORT = process.env.PORT;
 const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_HOST = process.env.DATABASE_HOST;
 const DATABASE_PORT = process.env.DATABASE_PORT;
+const JWT_TOKEN = process.env.JWT_TOKEN;
+const SALT_FACTOR = parseInt(process.env.SALT_FACTOR);
 
 module.exports = {
+  PORT,
   DATABASE_USERNAME,
   DATABASE_PASSWORD,
   DATABASE_HOST,
   DATABASE_PORT,
+  JWT_TOKEN,
+  SALT_FACTOR,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-// Constants
-const PORT = process.env.PORT || "3000";
-const DATABASE_USERNAME = process.env.DATABASE_USERNAME || "username";
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "password";
-const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
-const DATABASE_PORT = process.env.DATABASE_PORT || "27017";
+const {
+  PORT,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = require("./config/constants");
 
 mongoose
   .connect(
